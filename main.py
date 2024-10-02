809d2915e7d4bf25eb9c7181d4a4cf04aae2d3a5
Zdenko Kanoš <xkanos@stuba.sk>
2024-10-02 14:19:03 +0200

    made some adjustments

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import random
 import math
 import tkinter
+import matplotlib.pyplot as plt
 
 root = tkinter.Tk()
 root.title("Town MAP")
@@ -11,6 +12,7 @@ canvas.pack()
 
 DELAY = 0
 tabu_list = []
+graph_array = []
 
 def clear_canvas(iteration_count):
     canvas.delete("all")
@@ -20,20 +22,22 @@ def clear_canvas(iteration_count):
 
 # creates visible connections between cities
 def create_line(x1, y1, x2, y2, color):
-    global DELAY
     canvas.create_line(x1, y1, x2, y2, fill=color)
     canvas.after(DELAY)
     canvas.update()
 
-def show_best_path(best_path, shortest_dist):
+# this function shows visualization of the best path found during the algorithm search
+def show_best_overall(best_overall):
+    shortest_dist = fitness(best_overall)
     canvas.delete("all")
     canvas.create_text(200, 40, text=f"Best Path Length: {shortest_dist:.2f}", font=("Arial", 22), fill="black")
     for town in town_coordinates:
         canvas.create_oval(town[0] - 5, town[1] - 5, town[0] + 5, town[1] + 5, fill="red")
-    for i in range(len(best_path) - 1):
-        create_line(best_path[i][0], best_path[i][1], best_path[i + 1][0], best_path[i + 1][1], "black")
-    create_line(best_path[len(best_path) - 1][0], best_path[len(best_path) - 1][1], best_path[0][0], best_path[0][1], "blue")
+    for i in range(len(best_overall) - 1):
+        create_line(best_overall[i][0], best_overall[i][1], best_overall[i + 1][0], best_overall[i + 1][1], "black")
+    create_line(best_overall[len(best_overall) - 1][0], best_overall[len(best_overall) - 1][1], best_overall[0][0], best_overall[0][1], "blue")
 
+# this function creates lines between nodes (cities), last line is blue to be sure it came back to starting point
 def create_connections(path, i):
     clear_canvas(i)
     n = len(path)
@@ -63,6 +67,7 @@ def town_init(n):
         town_coordinates.append([x, y])
     return town_coordinates
 
+# this is the fitness function
 def fitness(path):
     total_distance = 0
     n = len(path)
@@ -76,16 +81,15 @@ def update_tabu_list(local_max):
     max_tabu_size = 10
     if len(tabu_list) >= max_tabu_size:
         tabu_list.pop(0)
-    if local_max not in tabu_list:
-        tabu_list.append(local_max)
+    tabu_list.append(local_max)
 
-def evaluation(total_distance, path, best_path, shortest_dist):
-    if total_distance < shortest_dist:
-        shortest_dist = total_distance
-        best_path = path.copy()
-        return 0, best_path, shortest_dist
-    return 1, best_path, shortest_dist
+def evaluation(path, best_overall):
+    if fitness(path) < fitness(best_overall):
+        best_overall = path.copy()
+        return 0, best_overall
+    return 1, best_overall
 
+# this function makes mutations of the path
 def change_neighbours(arr):
     neighbour = arr.copy()
     n = len(neighbour)
@@ -99,46 +103,51 @@ def change_neighbours(arr):
     return neighbour
 
 def tabu_search_alg(init_path, n):
-    maximum = 999999999
-    max_iterations = 5000
+    max_iterations = 1000
     max_no_improvement = 50
     end_count = 0
-    shortest_dist = maximum
-    best_local_dist = maximum
-    best_path = init_path
-    best_local_path = []
+    best_overall = init_path
+    candidate = init_path
+    best_candidate = init_path
+    update_tabu_list(best_candidate)
     for i in range(max_iterations):
-        for j in range(n):
-            path = change_neighbours(best_path)
-            total_distance = fitness(path)
-            if total_distance < best_local_dist:
-                best_local_dist = total_distance
-                best_local_path = path.copy()
-        update_tabu_list(best_local_path)
-        result, best_path, shortest_dist = evaluation(best_local_dist, best_local_path, best_path, shortest_dist)
-        print(best_local_dist)
-        best_local_dist = maximum
-        if result == 0:
+        for j in range(5):  # I create n individuals, which are not in tabu list
+            path = change_neighbours(best_candidate)
+            if fitness(path) < fitness(best_candidate):  # choose the best one
+                candidate = path.copy()
+        if fitness(candidate) < fitness(best_candidate):    # if the individual from the chosen is
+            best_candidate = candidate.copy()               # better than actual I start to search in it
+        result, best_overall = evaluation(best_candidate, best_overall)  # I check if the value is best from all so far
+        print(fitness(candidate))
+        graph_array.append(fitness(best_candidate))
+        update_tabu_list(best_candidate)
+        if result == 0:  # this is just to keep track of not improving anymore
             end_count = 0
         else:
             end_count += result
-        if end_count >= max_no_improvement:
-            print("stopped")
+        if end_count == max_no_improvement:
             break
-        if i % 100 == 0:
-            create_connections(best_local_path, i)
-    show_best_path(best_path, shortest_dist)
-    return shortest_dist
+        if i % 100 == 0:  # creates visualisation of every 200th path
+            create_connections(best_candidate, i)
+    show_best_overall(best_overall)
+    return fitness(best_overall)
 
+def plot_graph():
+    plt.plot(graph_array)
+    plt.title('Total Distance over Iterations')
+    plt.xlabel('Iterations')
+    plt.ylabel('Total Distance')
+    plt.grid(True)
+    plt.show()
 
-N = 100
+N = 20
 town_coordinates = town_init(N)
-# town_coordinates =[(60, 200), (180, 200), (100, 180), (140, 180),
-#     (20, 160), (80, 160), (200, 160), (140, 140),
-#     (40, 120), (120, 120), (180, 100), (60, 80),
-#     (100, 80), (180, 60), (20, 40), (100, 40),
-#     (200, 40), (20, 20), (60, 20), (160, 20)]
+# town_coordinates = [(94, 390), (348, 390), (179, 359), (263, 359),
+#     (10, 328), (137, 328), (390, 328), (263, 297),
+#     (52, 266), (221, 266), (348, 234), (94, 203),
+#     (179, 203), (348, 172), (10, 141), (179, 141),
+#     (390, 141), (10, 110), (94, 110), (306, 110)]
 random.shuffle(town_coordinates)  # ensures first iteration is randomly created
 print("Shortest distance: ", tabu_search_alg(town_coordinates, N))
-
+plot_graph()
 root.mainloop()
